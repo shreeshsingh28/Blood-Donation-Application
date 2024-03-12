95737661b72ede2c808d0a6e27088f1981de5902
Shreesh Singh <96125891+shreeshsingh28@users.noreply.github.com>
2024-03-12 15:25:54 +0530

    Login UI

--- app/src/main/java/com/example/blooddonationapplication/MainActivity.kt
+++ app/src/main/java/com/example/blooddonationapplication/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.blooddonationapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btn1 = findViewById<Button>(R.id.btn1)
+
+        btn1.setOnClickListener {
+            val intent = Intent(this, LoginPage::class.java)
+            startActivity(intent)
+        }
+
     }
 }
